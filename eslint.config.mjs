d2f6fd692964b10b72cb42579b896cb7b5a51b9d
Harshit Bamotra <harshitbamotra.01@gmail.com>
2024-10-17 02:16:30 +0530

    Basic IOREDIS setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,8 @@ export default [
     ...tseslint.configs.recommended,
     {
         rules: {
-            // "no-unused-vars": ["off"],
+            '@typescript-eslint/no-unused-vars': 'off',
+            "no-unused-vars": ["off"],
             'no-var': 'error',
             'semi': ['error', 'always'],
             'indent': ['error', 4, { SwitchCase: 4 }],
